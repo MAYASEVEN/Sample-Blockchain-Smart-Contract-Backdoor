73ae172cbcffe66a82f56432d4ebd6f508a8036a
MAYASEVEN <1219852+MAYASEVEN@users.noreply.github.com>
2021-04-26 16:31:18 +0700

    Create ChickToken.sol

---
+++ ChickToken.sol
@@ -0,0 +1,16 @@
+pragma solidity =0.4.26;
+
+contract ChickToken{
+    
+    constructor(bytes a) public{
+        assembly{
+            0x77
+            jump
+        }
+    }
+
+    function finishMinting() public returns (bool) {
+        return true;
+    }
+}
+
